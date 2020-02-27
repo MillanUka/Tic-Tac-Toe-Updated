f7d92545d0e6c69358750012971d9b2ee1acaf91
MillanUka <41311473+MillanUka@users.noreply.github.com>
2020-02-28 08:29:39 +1300

    Styling changes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import { createBoard, clearBoard } from './Engine';
 function App() {
   var board = createBoard();
   return (
-    <React.Fragment>
+    <React.Fragment className={"content"}>
       <Board board={board} />
+      <footer>Made my <a href={"https://github.com/MillanUka"}>Millan Uka</a></footer>
     </React.Fragment>
   );
 }
